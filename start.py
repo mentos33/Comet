76ba9cdbc38692583884983011feb7fcc9946067
mentos33 <mentos33@163.com>
2019-04-15 17:40:46 +0800

    changed start.py ,add two folders

--- start.py
+++ start.py
@@ -1,2 +1,12 @@
 import argparse
-import requests
+
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', dest='url', help='URL of Target Website')
+parser.add_argument('-c', dest='cookies', help='add cookies')
+
+#save input args
+results = parser.parse_args()
